4aa8276d990b1285e0c0cb28f73e44e6affa71b0
freddyescobar169 <freddyescobar169@gmail.com>
2020-09-24 12:04:40 -0600

    adding source to firebase.json

--- functions/Deploy.ps1
+++ functions/Deploy.ps1
@@ -1,3 +1,8 @@
+<#
+    Snippet to run this script:
+    -firebBaseToken 1//0dZb0O_XivzwxCgYIARAAGA0SNwF-L9Ir9xkCsHqXF0PkA3LojiAg9XnqXP51BRGTb65dNouopwGtVT7Eu5rEIc-3UveM_-vOygk -project start-kleen-test -releaseMessage "TestRelease"
+#>
+
 param(
     [string]$fireBaseToken,
     [string]$fireBaseProject,
